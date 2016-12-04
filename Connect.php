9e2df5efcfeacaf522bd3b18aa11fbb180829335
SeanCarroll1 <carroll.seann@gmail.com>
2016-12-04 19:19:47 +0000

    Update

--- Connect.php
+++ Connect.php
@@ -5,53 +5,87 @@
  * Date: 03/12/2016
  * Time: 22:42
  */
-
-//perform connection
-$con=mysqli_connect("localhost","root","","students");
-
-//check if connection is successful
-if (mysqli_connect_errno())
+class Connection
 {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
 
-echo "Success <br>";
+    public static function getConnection($host, $username,$password,$dbName)
+    {
+        //perform connection
+        $mysqli=new mysqli($host,$username,$password,$dbName);
 
-//execute the SQL query and return records
-$result = mysqli_query($con,"SELECT * FROM registration");
+        //check if connection is successful
+        if (mysqli_connect_errno())
+        {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
 
-//fetch the data from the database
-while ($row = mysqli_fetch_array($result)) {
-    echo "ID:".$row{'id'}." Name:".$row{'first'}." 
-   ".$row{'last'}." Age:".$row{'age'}."<br>";
-}
-//get max id in table
-$getMax = mysqli_query($con,"SELECT MAX(id) FROM registration");
-$row = mysqli_fetch_row($getMax);
-$max = $row[0];
-
-//loop inserting values into database
-for ($x = 0; $x <= 10; $x++) {
-    $max++;
-    $sql = "INSERT INTO registration (id,first, last, age)
-VALUES ($max,'Alex', 'Sui Cen', 23)";
-
-
-    if ($con->query($sql) === TRUE) {
-        echo "New record created successfully <br>";
-    } else {
-        echo "Error: " . $sql . "<br>" . $con->error;
+        echo "Successfully Connected <br>";
+        return $mysqli;
     }
 
+    public static function printValues($con,$tableName)
+    {
+
+                //execute the SQL query and return records
+                $result = mysqli_query($con,"SELECT * FROM $tableName");
+
+                //fetch the data from the database
+                while ($row = mysqli_fetch_array($result))
+                {
+                    echo "ID:".$row{'id'}." Name:".$row{'first'}." 
+                            ".$row{'last'}." Age:".$row{'age'}."<br>";
+                }
+
+
+    }
+
+    public static function InsertMultipleValues($mysqli,$firstName,$lastName,$age,$insertCount)
+    {
+        //get max id in table
+        $getMax = mysqli_query($mysqli,"SELECT MAX(id) FROM registration");
+        $row = mysqli_fetch_row($getMax);
+        $max = $row[0];
+
+        $stmt = $mysqli->prepare("INSERT INTO registration VALUES (?, ?, ?, ?)");
+        $stmt->bind_param('sssd',$max, $firstName, $lastName, $age);
+
+        //loop inserting values into database
+        for ($x = 0; $x <= $insertCount; $x++)
+        {
+            $max++;
+
+            //will add in values each loop from the variables above
+            if ($stmt->execute())
+            {
+                echo "Successfully entered. Row inserted: $stmt->affected_rows <br>";
+            }else
+            {
+                echo $stmt->error;
+            }
+
+        }
+
+        $stmt->close();
+
+    }
+
+    public static function DeleteValues($con,$tableName,$value)
+    {
+        //Delete variable matching the value passed
+        $con->query("DELETE FROM $tableName WHERE first = '$value'");
+        printf("Affected rows (DELETE): %d\n", $con->affected_rows);
+
+    }
+
+
 }
 
-$sql2 =mysqli_query($con, "DELETE FROM registration WHERE first = 'Zaid'");
+$test=new Connection();
+$con=$test->getConnection("localhost","root","","students");
+$test ->printValues($con,"registration");
+$test ->InsertMultipleValues($con,"Sean","Carroll",23,5);
+$test ->DeleteValues($con,"registration","Sean");
 
-if ($con->query($sql2) === TRUE) {
-    echo "Deleted records <br>";
-} else {
-    echo "Error: " . $sql2 . "<br>" . $con->error;
-}
 
 
 $con->close();
